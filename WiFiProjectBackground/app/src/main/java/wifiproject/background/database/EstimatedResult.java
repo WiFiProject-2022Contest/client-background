1a742d1455528e65ce183685e4ab3ea3bb735f94
geniemo <kor98won@gmail.com>
2022-07-25 16:19:22 +0900

    [refactor] 생성자 리팩터링

--- WiFiProjectBackground/app/src/main/java/wifiproject/background/database/EstimatedResult.java
+++ WiFiProjectBackground/app/src/main/java/wifiproject/background/database/EstimatedResult.java
@@ -57,74 +57,58 @@ public class EstimatedResult {
     private Date date;
 
     private Integer _new;
-
-    public EstimatedResult() {
-        this.building = null;
-        this.ssid = null;
+    
+    // PositioningAlgorithm에서만 사용함
+    @Ignore
+    public EstimatedResult(String building, String ssid, String uuid, String method, Integer k, Integer threshold, Integer algorithmVersion, Integer _new) {
         this.pos_x = 0d;
         this.pos_y = 0d;
         this.est_x = 0d;
         this.est_y = 0d;
-        this.uuid = null;
-        this.method = null;
-        this.k = -1;
-        this.threshold = -1;
-        this.algorithmVersion = 0;
+        this.building = building;
+        this.ssid = ssid;
+        this.uuid = uuid;
+        this.method = method;
+        this.k = k;
+        this.threshold = threshold;
+        this.algorithmVersion = algorithmVersion;
+        this._new = _new;
         this.date = new Date(System.currentTimeMillis());
     }
 
-    public EstimatedResult(EstimatedResult estimatedResult) {
-        this.building = estimatedResult.getBuilding();
-        this.ssid = estimatedResult.getSsid();
-        this.pos_x = estimatedResult.getPos_x();
-        this.pos_y = estimatedResult.getPos_y();
-        this.est_x = estimatedResult.getEst_x();
-        this.est_y = estimatedResult.getEst_y();
-        this.uuid = estimatedResult.getUuid();
-        this.method = estimatedResult.getMethod();
-        this.k = estimatedResult.getK();
-        this.threshold = estimatedResult.getThreshold();
-        this.algorithmVersion = estimatedResult.getAlgorithmVersion();
-        this.date = estimatedResult.getDate();
-    }
-
-    public EstimatedResult(String building, String ssid, String uuid) {
-        this();
-
+    public EstimatedResult(String building, String ssid,
+                           Double pos_x, Double pos_y, Double est_x, Double est_y,
+                           String uuid, String method, Integer k, Integer threshold, Integer algorithmVersion, Integer _new) {
         this.building = building;
         this.ssid = ssid;
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+        this.est_x = est_x;
+        this.est_y = est_y;
         this.uuid = uuid;
-    }
-
-    public EstimatedResult(String building, String ssid, String uuid, String method, int k, int threshold, int algorithmVersion) {
-        this(building, ssid, uuid);
-
         this.method = method;
         this.k = k;
         this.threshold = threshold;
         this.algorithmVersion = algorithmVersion;
+        this.date = new Date(System.currentTimeMillis());
+        this._new = _new;
     }
 
-    public EstimatedResult(String building, String ssid, String uuid, String method, int K, int threshold, int algorithmVersion, long date) {
-        this(building, ssid, uuid, method, K, threshold, algorithmVersion);
-
+    public EstimatedResult(String building, String ssid,
+                           Double pos_x, Double pos_y, Double est_x, Double est_y,
+                           String uuid, String method, Integer k, Integer threshold, Integer algorithmVersion, long date, Integer _new) {
+        this.building = building;
+        this.ssid = ssid;
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+        this.est_x = est_x;
+        this.est_y = est_y;
+        this.uuid = uuid;
+        this.method = method;
+        this.k = k;
+        this.threshold = threshold;
+        this.algorithmVersion = algorithmVersion;
         this.date = new Date(date);
-    }
-
-    public EstimatedResult(String building, String ssid, double positionRealX, double positionRealY, double positionEstimatedX, double positionEstimatedY,
-                           String uuid, String method, int k, int threshold, int algorithmVersion, long date) {
-        this();
-
-        this.building = building;
-        this.ssid = ssid;
-        this.pos_x = positionRealX;
-        this.pos_y = positionRealY;
-        this.est_x = positionEstimatedX;
-        this.est_y = positionEstimatedY;
-        this.uuid = uuid;
-        this.method = method;
-        this.k = k;
-        this.threshold = threshold;
-        this.algorithmVersion = algorithmVersion;
+        this._new = _new;
     }
 }
